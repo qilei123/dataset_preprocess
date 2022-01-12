ebe435c22fe9eb75ca9b63edaff69b922106c69c
qilei123 <qileimail123@gmail.com>
2022-01-12 01:06:34 -0500

    test

--- demo_show.py
+++ demo_show.py
@@ -5,15 +5,17 @@ import os
 import matplotlib.pyplot as plt
 
 
-ann_dir = 'annotations/t1_from_t2/all.json'
+ann_dir = '/home/qilei/.TEMP/teeth_data2/20211213_t3/annotations/instances_default.json'
 
-img_folder = ''
+img_folder = '/home/qilei/.TEMP/teeth_data2/20211213_t3/images/'
 
 coco = COCO(ann_dir)
 
 ImgId = 12
 
 img = coco.loadImgs([ImgId])[0]
+
+img['file_name'] = img_folder+img['file_name']
 
 plt.imshow(I); plt.axis('off')
 annIds = coco.getAnnIds(imgIds=img['id'])
