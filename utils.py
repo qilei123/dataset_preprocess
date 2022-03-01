5f909aa0270bb8c474e7c6289a41493b761272de
qilei123 <qileimail123@gmail.com>
2022-03-01 16:13:40 -0500

    change name test

--- utils.py
+++ utils.py
@@ -2,22 +2,38 @@ import os
 
 def change_video_names():
     records = open('temp_datas/changweijing_issues1.txt',encoding="utf8")
-    map_records = open('temp_datas/changweijing_issues_filenames_map.txt','w',encoding="utf8")
+    #map_records = open('temp_datas/changweijing_issues_filenames_map.txt','w',encoding="utf8")
     line = records.readline()
     count=0
     new_base_name = '20220301_1024_010'
+
+    video_dir = '/data3/qilei_chen/DATA/changjing_issues/'
+
     while line:
         
         line = line.replace('\n','')
         line_eles = line.split('	')
 
         if len(line_eles[0])>0:
-            new_line = line_eles[0]+" "+new_base_name+str(count)+"_"+line_eles[1]+"\n"
-            map_records.write(new_line)
+            new_line = line_eles[0]+" "+new_base_name+str(count)+"_"+line_eles[1]
+            new_line_eles = new_line.split(' ')
+            
+            if os.path.exists(os.path.join(video_dir,new_line_eles[0]+".avi")):
+                org_video_name = os.path.join(video_dir,new_line_eles[0]+".avi")
+            else:
+                org_video_name = os.path.join(video_dir,new_line_eles[0]+".mp3")
+  
+            #os.rename(org_video_name,org_video_name.replace(new_line_eles[0],new_line_eles[1]))
+            print(org_video_name)
+            print(org_video_name.replace(new_line_eles[0],new_line_eles[1]))
+
+            #map_records.write(new_line)
             count+=1
 
         line = records.readline()
     
+
+
 
 if __name__=="__main__":
     change_video_names()
